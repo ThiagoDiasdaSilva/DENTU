183228a245d23691c8b2e0200f5e15601b403fdd
Tr4k1nasofficial <cxstudying018@gmail.com>
2026-06-21 18:43:29 -0300

    refactor/Maintainability-Unexpected var-L4

--- static/website/js/default-assets/avoid.console.error.js
+++ static/website/js/default-assets/avoid.console.error.js
@@ -1,7 +1,7 @@
 // Avoid `console` errors in browsers that lack a console.
 (function () {
     let method;
-    var noop = function noop() {};
+    let noop = function noop() {};
     var methods = [
         'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
         'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
